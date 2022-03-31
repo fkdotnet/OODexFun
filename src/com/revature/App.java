21115e1a6adb4d10b9b3712d4d83bb4674cd6e80
Jacob <jacobmockler@gmail.com>
2022-03-31 14:14:44 -0400

    test

--- src/com/revature/App.java
+++ src/com/revature/App.java
@@ -9,7 +9,7 @@ public class App {
 		Scanner Scan = new Scanner(System.in);
 		System.out.println("Enter The Pokemon's Global Dex Number");
 		String choice = Scan.nextLine();
-		Pokemon Bulbasaur = new Pokemon("1", "Bulbasaur", "Grass", "A strange seed was planted on its back at birth.\n The plant sprouts and grows with this Pokémon. ", "Overgrow");
+		Pokemon Bulbasaur = new Pokemon("1", "Bulbasaur", "Grass", "A strange seed was planted on its back at birth.\nThe plant sprouts and grows with this Pokémon. ", "Overgrow");
 		Pokemon Charmander = new Pokemon("4", "Charmanderr", "Fire", "The fire on the tip of its tail is a measure of its life. If the Pokémon is healthy, its tail burns intensely. ", "Blaze");
 		Pokemon Pikachu = new Pokemon("25", "Pikachu", "Electric", "Possesses cheek sacs in which it stores electricity.\nThis clever forest-dweller roasts tough berries with an electric shock before consuming them.", "Static");
 		Pokedex.add(Pikachu);
